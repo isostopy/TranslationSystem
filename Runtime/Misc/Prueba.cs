efd79aafc31fa01b0d2eb73decfdbdbdfc051062
Sergio <software@isostopy.com>
2024-06-24 12:48:41 +0200

    Vuelta a añadir

---
+++ Runtime/Misc/Prueba.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("La prueba funciona");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
